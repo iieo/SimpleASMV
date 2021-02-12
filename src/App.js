90610666a27e7134e887761ed819aff9c2584cd3
iiiieo <achtbyte01@gmail.com>
2021-02-12 08:59:49 +0100

    new design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+import AceEditor from "react-ace";
+import CellsContainer from "./cells";
+import "ace-builds/src-noconflict/mode-assembly_x86";
+import "ace-builds/src-noconflict/theme-monokai";
+
+/*  https://github.com/securingsincity/react-ace  */
+
+function onChange(newValue) {}
 
 function App() {
+  let [cells, setCells] = React.useState([0, 0, 0, 0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <h1 className="heading">Simple ASMR</h1>
       </header>
+      <body className="body">
+        <AceEditor
+          mode="assembly_x86"
+          theme="monokai"
+          width="60vw"
+          height="calc(100vh - 80px)"
+          fontSize={20}
+          onChange={onChange}
+          name="codeEditor"
+          placeholder="Type your code here..."
+          editorProps={{ $blockScrolling: true }}
+          markers={[
+            {
+              startRow: 0,
+              startCol: 0,
+              endRow: 1,
+              endCol: 0,
+              className: "debug-marker",
+              type: "background",
+            },
+          ]}
+        />
+        <CellsContainer cells={cells} />
+      </body>
     </div>
   );
 }
---
+++ src/cells.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import React from "react";
+
+function comp() {
+  return (
+    <div>
+      <CellsContainer larslol={{ 1: "test" }}></CellsContainer>
+    </div>
+  );
+}
+
+class CellsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.cells = [];
+    this.createCells();
+  }
+  createCells = () => {
+    for (let i = 0; i < this.props.cells.length; i++) {
+      this.cells.push(<Cell value={this.props.cells[i]} id={i}></Cell>);
+    }
+  };
+  render() {
+    return <div className="cellContainer">{this.cells}</div>;
+  }
+}
+
+class Cell extends React.Component {
+  render() {
+    return (
+      <div className="cell">
+        <p className="cellTextNo">R #{this.props.id}</p>
+        <p className="cellText">{this.props.value}</p>
+      </div>
+    );
+  }
+}
+
+export default CellsContainer;
